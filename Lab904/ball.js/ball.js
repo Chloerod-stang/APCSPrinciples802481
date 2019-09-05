21f56fcc7041abf578b3391b29c018d8220d119d
Chloerod-stang <54385645+Chloerod-stang@users.noreply.github.com>
2019-09-04 19:41:27 -0700

    Update ball.js

--- Lab904/ball.js/ball.js
+++ Lab904/ball.js/ball.js
@@ -3,6 +3,20 @@
 class Ball{
   constructor(x,y,dx,dy){
     this.loc = createVector(x,y);
-    this.vel = createVector
+    this.vel = createVector(dx,dy);
+    this.acc = createVector(0,0);
+    this.clr = color (random(255), random(255), random(255));
 }
+run(){
+  this.checkEdges();
+  this.update();
+  this.render();
 }
+run(){
+  this.checkEdges();
+  this.update();
+  this.render();
+}
+  checkEdges(){
+    this.vel(dx) = -1*this.vel(dx)
+    if(this.loc(y)
