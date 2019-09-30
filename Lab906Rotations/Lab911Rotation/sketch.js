6af682a53d06d8099090c811d8cd512acdd987b3
Chloerod-stang <54385645+Chloerod-stang@users.noreply.github.com>
2019-09-29 23:25:48 -0700

    no main ship, paddle, or ball

--- Lab906Rotations/Lab911Rotation/sketch.js
+++ Lab906Rotations/Lab911Rotation/sketch.js
@@ -3,7 +3,11 @@
 
 //  The setup function function is called once when your program begins
 var ships =[]
-var attractor, repeller, mainBall, paddle, mainShips;
+var attractor;
+var repeller;
+// var mainBall;
+// var paddle;
+// var mainShips;
 
 function setup() {
   var cnv = createCanvas(800,800);
@@ -14,20 +18,22 @@ function setup() {
 
 function draw() {
   background(10, 10, 10, 50)
-  mainShips.run();
+  // mainShips.run();
   runShips();
-  runPaddle();
+  // runPaddle();
 }
 
 function loadShips(n) {
   fill(80, 46, 255)
-  mainShips = new Ships(width/2, height/2, random(-2,2), ramdom(-2,2), 40, 40)
-  paddle = new Paddle(random(width), random(height), 20, 10)
-  for(var i = 0; i<n; i++)
-    ships[i] = newShips(random(width), random(height), random(-5,5), random(-5,5), random(0,TWO_PI))
+  // mainShip = new Ship(width/2, height/2, random(-2,2), ramdom(-2,2))  // 40, 40
+  // paddle = new Paddle(random(width), random(height), 20, 10)
+  for(var i = 0; i<n; i++) {
+    ships[i] = new Ship(random(0,200), random(0,200), random(-5,5), random(-5,5)) // random(0,TWO_PI)
+  }
 }
+
 function runShips(){
   for(var i = 0; i < ships.length; i++){
-  ships[i].run();
+    ships[i].run();
   }
 }
