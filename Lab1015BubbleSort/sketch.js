3f8ac4cba4ae0105425444346984f7d676fdb752
Chloerod-stang <54385645+Chloerod-stang@users.noreply.github.com>
2019-10-15 08:46:59 -0700

    1015

--- Lab1015BubbleSort/sketch.js
+++ Lab1015BubbleSort/sketch.js
@@ -1,17 +1,30 @@
-//  Your Name
-// 	Date or version number
+//  Chloe Rodriguez Stangle
+// 	10-15-19
 //  This is a comment
 //  The setup function function is called once when your program begins
+
+var numList = [];
+
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
 
-
+  loadArray();
+  sway();
 }
 
-//  The draw function is called @ 30 fps
-function draw() {
+function sway();
+var temp = list[a]
+list[a] = list [b]
+list[b] = temp
+
+function loadArray(){
+  for(var i = 1; i <5; i++){
+  for(var j = i; j< 1; j++){
+    console.log
+  }
+  }
 
 }
