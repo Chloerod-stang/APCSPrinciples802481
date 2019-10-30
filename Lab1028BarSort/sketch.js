201cf0482755a050fdcd63ac9dff92cf463d6611
Chloerod-stang <54385645+Chloerod-stang@users.noreply.github.com>
2019-10-30 08:51:34 -0700

    1030

--- Lab1028BarSort/sketch.js
+++ Lab1028BarSort/sketch.js
@@ -2,14 +2,14 @@
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
-var bars=[];
+var bars = [];
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  loadBars(20)
-  frameRate()
+  loadBars(20);
+  //frameRate()
   for(var i=0; i<bars.length; i++){
     bars[i].run();
   }
@@ -20,12 +20,17 @@ function draw() {
 bubbleSort()
 drawList(bars)
 }
-function inisite(n){
-
+function loadBars(n){
+  for (var j=0; j<bars.length-1; j++){
+    
+    }
+  }
 }
 function drawList(listBars){
   for(var i = arr.length - 1; i > 0; i--){
       for(var j = 0; j<1; j++){
         swap(arr, j, j+1)
 
+}
+}
 }
