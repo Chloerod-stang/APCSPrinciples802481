ef6c07ed21071bd7ed73fa92c1af1e0b04026c02
Chloerod-stang <54385645+Chloerod-stang@users.noreply.github.com>
2019-11-07 08:49:57 -0800

    1007

--- 1031SnakeGame/sketch.js
+++ 1031SnakeGame/sketch.js
@@ -19,8 +19,6 @@ function setup() {
 function draw() {
   background(20, 50, 100);
   snake.run();
-  for(var i - 0; i< snake.body.length; i++)
-
   //food.run();
   //newGame();
   //startNewRound();
