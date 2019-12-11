8b8fc9cccbb1a64f00ba30a42f01656de0cab1c4
Chloerod-stang <54385645+Chloerod-stang@users.noreply.github.com>
2019-12-10 16:24:45 -0800

    Update planet.js

--- ShipThing1202/planet.js
+++ ShipThing1202/planet.js
@@ -1,30 +1,44 @@
 
-class Planet{
-  constructor(x,y,dx,dy){
-    this.loc = createVector(x,y);
-    this.vel = createVector(dx,dy);
-    this.acc = createVector(0,0);
-    this.clr = color (random(255), random(255), random(255));
-}
-run(){
-  this.checkEdges();
-  this.update();
-  this.render();
-// }
-// checkEdges(){
-//   this.vel(dx)=-1*this.vel(dx)
-//   this.vel(dx) = -1*this.vel(dx);
-//   this.vel(dy) = -1*this.vel(dy);
-//   this.vel(dy)=this.vel(dy)
-// }
-//   update(){
-//
-//     this.loc.x=this.loc.x+this.vel.dx;
-//     this.loc.y=this.loc.y+this.vel.dy;
-//   }
-//   render(){
-//     fill(this.clr);
-//     ellipse(this.loc.x, this.loc.y)
+class Planet {
+ constructor(x, y, dx, dy, id) {
+   this.clr = color(random(255), random(255), random(255));
+   this.loc = createVector(x, y);
+   this.vel = createVector(dx, dy);
+   this.id = id;
+   this.acc = createVector(0, .1);
+ 
+   this.run = function () {
+     this.checkEdges();
+     this.update();
+     this.render();
    }
-//
+   this.checkEdges = function () {
+     if (this.loc.x < 0) {
+       this.vel.x = -this.vel.x*random()*3; //left
+     }
+     if (this.loc.x > width) { //right
+       this.vel.x = -this.vel.x*random()*2;
+     }
+     if (this.loc.y < 0) { //down
+       this.vel.y = -this.vel.y*random()*2;
+     }
+     if (this.loc.y > height) { //up
+       this.vel.y = -this.vel.y*random()*2;
+     }
+ 
+     var dist = this.loc.dist(ships[0].loc);
+     if(dist < 80){
+       this.loc.x = Math.floor((Math.random() * 700) + 50);
+       this.loc.y = Math.floor((Math.random() * 700) + 50);
+     }
+   }
+   this.update = function () {
+       this.vel.limit(2);
+      this.loc.add(this.vel);
+   }
+   this.render = function () {
+   fill(this.clr);
+   ellipse(this.loc.x, this.loc.y, 50, 50);
+ }
+}
 }
