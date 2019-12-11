f3d72d180af8577dbfb3fbdd535ccb1233766a90
Chloerod-stang <54385645+Chloerod-stang@users.noreply.github.com>
2019-12-10 16:26:35 -0800

    Update ship.js

--- ShipThing1202/ship.js
+++ ShipThing1202/ship.js
@@ -1,50 +1,54 @@
 
-class Ship{
+class ship {
+   constructor(x, y, dx, dy, id) {
+     this.clr = color(random(255), random(255), random(255));
+     this.loc = createVector(x, y);
+     this.vel = createVector(dx, dy);
+     this.id = id;
+     this.acc = createVector(0, .1);
+      this.run = function () {
+       this.checkEdges();
+       this.update();
+       this.render();
+     }
+      this.checkEdges = function () {
+       if (this.loc.x < 0) {
+         this.loc.x = 800; //left
+       }
+       if (this.loc.x > width) { //right
+         this.loc.x = 0;
+       }
+       if (this.loc.y < 0) { //down
+         this.loc.y = 800;
+       }
+       if (this.loc.y > height) { //up
+         this.loc.y = 0;
+       }
+     }
+     this.update = function () {
+       if (id > -1) {
+         var dist = this.loc.dist(planet.loc);
+         if (dist > 11&&dist < 350) {
+           this.Force = p5.Vector.sub(planet.loc, this.loc);//attract
+           this.Force.normalize();
+           this.Force.mult(0.6);
+           this.vel.add(this.Force);
+           this.vel.add(this.acc);
+         }
+         this.loc.add(this.vel); //add velocity to make gravity
+       }
+         this.vel.limit(2);
+        this.loc.add(this.vel);
+     }
+     this.render = function () {
+     fill(this.clr);
+     push();//changes dont matter
+     let myHeading = this.vel.heading();//
+     translate(this.loc.x, this.loc.y)//for orgin
+     rotate(myHeading, 0);//rotate hopefully to where it is pointing
+     triangle(-10, 10, -10, -10, 10, 0);//make perfet triangle
+     pop();//things matter again
+   }
+ }
+}
 
-  constructor(x,y,dx,dy){
-    this.loc = createVector(x,y);
-    this.vel = createVector(dx,dy);
-    this.acc = createVector(0,0);
-    this.clr = color(random(255), random(255), random(255));
-  }
-
-  run(){
-    this.checkEdges();
-    this.update();
-    this.render();
-  }
-
-  checkEdges(){
-    if (this.loc.x> width){
-      this.vel.x = -this.vel.x
-    }
-
-    if (this.loc.x < 0){
-      this.vel.x = -this.vel.x
-    }
-
-    if (this.loc.y > height){
-      this.vel.y = -this.vel.y
-    }
-
-    if (this.loc.y<0){
-      this.vel.y = -this.vel.y
-    }
-  }
-
-  update(){
-    this.vel.add(this.acc)
-    this.loc.add(this.vel)
-      // this.angle += TWO_PI * 0.01
-  }
-
-  render(){
-    this.angle = this.vel.heading() + PI/2;
-    push()
-      translate(this.loc.x, this.loc.y);
-//      rotate(this.angle);
-      fill(this.clr);
-      triangle(-5, 5, 5, 5, 0, -10)
-    pop();
-  }
-}// ++++++++++++++++++++++++++++++++++++  end ship
